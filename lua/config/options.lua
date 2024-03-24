edb34dc1c21d9f8794cb0a11859c82cfa92aec22
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-03-24 21:15:26 +0700

    chore: turn off relative number

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- NVIM CONFIG --
+vim.opt.relativenumber = false
+
 -- NEOVIDE CONFIG --
 vim.g.neovide_input_macos_alt_is_meta = true
