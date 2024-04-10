67ea2f7511b2a6c36852373f5afb7cc299fb55a7
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-04-10 16:15:09 +0700

    chore: disable nvim notify

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "folke/flash.nvim", enabled = false },
   { "linux-cultist/venv-selector.nvim", enabled = false },
   { "windwp/nvim-ts-autotag", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
 }
